ccc39bce9338c5cfd1c87750a1da43dcf0eab370
Perabjoth Singh Bajwa <perabjothsinghbajwa@gmail.com>
2024-05-12 20:52:16 -0400

    random fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,13 @@ module.exports = {
         icon: 'src/images/favicon.png',
       },
     },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        purgeCSSOptions: {
+        },
+      },
+    },
   ],
 };
